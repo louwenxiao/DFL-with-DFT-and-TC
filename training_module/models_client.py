3c85fc12e98561a0204f55b31d127a80493b75b5
娄文霄 <86142265+louwenxiao@users.noreply.github.com>
2021-12-24 22:05:15 +0800

    Update models_client.py

--- training_module/models_client.py
+++ training_module/models_client.py
@@ -1,3 +1,32 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@louwenxiao 
+louwenxiao
+/
+P2P_KD
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+P2P_KD/client_module/models_client.py /
+@louwenxiao
+louwenxiao Update models_client.py
+Latest commit 06b345e in 26 seconds
+ History
+ 1 contributor
+102 lines (85 sloc)  3.35 KB
+   
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
@@ -53,117 +82,61 @@ class classify_VGG9(nn.Module):
         return e,x
     
 
-class ResidualBlock(nn.Module):
-    def __init__(self, inchannel, outchannel, stride=1):
-        super(ResidualBlock, self).__init__()
-        self.left = nn.Sequential(
-            nn.Conv2d(
-                inchannel,
-                outchannel,
-                kernel_size=3,
-                stride=stride,
-                padding=1,
-                bias=False,
-            ),
-            nn.BatchNorm2d(outchannel),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(
-                outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=False
-            ),
-            nn.BatchNorm2d(outchannel),
-        )
-        self.shortcut = nn.Sequential()
-        if stride != 1 or inchannel != outchannel:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    inchannel, outchannel, kernel_size=1, stride=stride, bias=False
-                ),
-                nn.BatchNorm2d(outchannel),
-            )
-
-    def forward(self, x):
-        out = self.left(x)
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-class ResNet(nn.Module):
-    def __init__(self, residual_block, num_classes=10):
-        super(ResNet, self).__init__()
-        self.inchannel = 16
-        self.layer1 = self.make_layer(residual_block, 16, 2, stride=1)
-        self.layer2 = self.make_layer(residual_block, 128, 2, stride=2)
-        self.layer3 = self.make_layer(residual_block, 256, 2, stride=2)
-        self.layer4 = self.make_layer(residual_block, 512, 2, stride=2)
-        self.fc = nn.Linear(512, num_classes)
-
-    def make_layer(self, block, channels, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)  # strides=[1,1]
-        layers = []
-        for stride in strides:
-            layers.append(block(self.inchannel, channels, stride))
-            self.inchannel = channels
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = self.layer1(x)        # 需要输入变化类型
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-def ResNet18():
-    return ResNet(ResidualBlock)
-
-class feature_extraction(nn.Module):
-    def __init__(self):
-        super(feature_extraction,self).__init__()
-        self.inchannel = 64
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-        )
     
-    def forward(self, x):
-        out = self.conv1(x)
-        return out
+class AlexNet(nn.Module):
+    def __init__(self):
+        super(AlexNet, self).__init__()
+        self.features1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            
+        )
+        self.features2 = nn.Sequential(
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 2 * 2, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x,z=None):
+            
+        e = x
+        if x.size()[1] != 64:       # 如果输入参数为特征，那么不在使用conv_layer，而是直接从下一步计算
+            x = self.features1(x)
+            e = x
+        x = self.features2(x)
+        x = x.view(-1, 256 * 2 * 2)
+        x = self.classifier(x)
+        return e,x
+        
+
     
 def create_model_instance(dataset_type, model_type):
     return classify_VGG9()
 
-    if model_type == "full":
-        return ResNet18()
-    
-    if model_type == 'resnet':
-        return feature_extraction(),ResNet18()      # 直接返回：特征提取和分类器
-    else:
-        return feature_extraction_VGG9(),classify_VGG9()
-    
-    if model_type == "feature_extraction":
-        return feature_extraction()
-    else:
-        return ResNet18()
-
-
-# class classify(nn.Module):
-#     def __init__(self, num_classes=10):
-#         super(classify, self).__init__()
-#         self.conv_layer = nn.Sequential(
-#             # Conv Layer block 1
-#             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2)
-#         )
-    
-    
-#     def forward(self, x):
-#         x = self.conv_layer(x)
-#         return x
-# model = classify()
-# print(model.state_dict().keys())
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
