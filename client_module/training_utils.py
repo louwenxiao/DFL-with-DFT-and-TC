2e613324c181c70b7891df251acb3deb093a89ec
娄文霄 <86142265+louwenxiao@users.noreply.github.com>
2022-02-26 17:48:26 +0800

    Update training_utils.py

--- client_module/training_utils.py
+++ client_module/training_utils.py
@@ -23,13 +23,15 @@ def train(model, data_loader, optimizer, local_iters=None, device=torch.device("
     if local_iters is None:
         local_iters = math.ceil(len(data_loader.loader.dataset) / data_loader.loader.batch_size)
     print("local_iters: ", local_iters)
+    
+    num = list(range(local_iters))
+    sample = random.sample(num, num_data)
 
     train_loss = 0.0
     samples_num = 0
     tensor_data = []
     for iter_idx in range(local_iters):
         data, target = next(data_loader)
-        
         data, target = data.to(device), target.to(device)
         x_data,output = model(data)
 
@@ -40,13 +42,13 @@ def train(model, data_loader, optimizer, local_iters=None, device=torch.device("
 
         train_loss += (loss.item() * data.size(0))
         samples_num += data.size(0)
-        x_data, output, target = x_data.to(torch.device("cpu")),output.to(torch.device("cpu")), target.to(torch.device("cpu"))
-        if len(tensor_data) < num_data:
-            if len(tensor_data) == 0:tensor_data.append((x_data, output, target))
-            elif random.random() < 0.5:tensor_data.append((x_data, output, target))
+        x_data, target = x_data.to(torch.device("cpu")), target.to(torch.device("cpu"))
+        if iter_idx in sample:
+            tensor_data.append((x_data, target))
     
-    key = ['features.0.weight', 'features.0.bias', 'features.3.weight', 'features.3.bias', 'features.6.weight', 
-           'features.6.bias', 'features.8.weight', 'features.8.bias', 'features.10.weight', 'features.10.bias']
+    key = ['features.0.weight', 'features.0.bias', 'features.3.weight', 'features.3.bias']
+    # key = ['features.0.weight', 'features.0.bias', 'features.3.weight', 'features.3.bias', 'features.6.weight', 
+    #        'features.6.bias', 'features.8.weight', 'features.8.bias', 'features.10.weight', 'features.10.bias']
     model_dict = dict()
     for para in model.state_dict().keys():
         if para in key:
@@ -86,7 +88,6 @@ def test(model, data_loader, device=torch.device("cpu"), model_type=None):
 
 def train_neighbor_data(model, data_loader, optimizer, device=torch.device("cpu")):
     model.train()
-    kl_loss = KL_Loss()
 
     train_loss = 0.0
     samples_num = 0
@@ -96,29 +97,16 @@ def train_neighbor_data(model, data_loader, optimizer, device=torch.device("cpu"
         raw_data.extend(d)
     np.random.shuffle(raw_data)
     for i in range(1):
-        for x_data, logit, label in raw_data:
-
-    # for data in data_loader:
-    #     for x_data, logit, label in data:
+        for x_data,label in raw_data:
         
-            x_data, logit, label = x_data.to(device), logit.to(device), label.to(device)
-            # print(x_data)
-            # print(label)
-            
+            x_data,label = x_data.to(device), label.to(device)
+
             _,output = model(x_data)
 
             optimizer.zero_grad()
-        
-            alpha = 1
-            whether_distill_on_the_server = 0
             
-            if whether_distill_on_the_server == 1:
-                loss_kd = kl_loss(output, logit).to(device)
-                loss_true = CE_loss(output, label).to(device)
-                loss = loss_kd + alpha * loss_true
-            else:
-                loss_true = CE_loss(output, label).to(device)
-                loss = loss_true
+            loss_true = CE_loss(output, label).to(device)
+            loss = loss_true
             loss.backward()
             optimizer.step()
         # print(label)
@@ -127,19 +115,5 @@ def train_neighbor_data(model, data_loader, optimizer, device=torch.device("cpu"
         train_loss /= samples_num
     
     return train_loss
-
-# KL损失函数
-class KL_Loss(nn.Module):
-    def __init__(self, temperature=3.0):
-        super(KL_Loss, self).__init__()
-        self.T = temperature
-
-    def forward(self, output_batch, teacher_outputs):
-        output_batch = F.log_softmax(output_batch / self.T, dim=1)
-        teacher_outputs = F.softmax(teacher_outputs / self.T, dim=1) + 10 ** (-7)
-
-        loss = self.T * self.T * nn.KLDivLoss(reduction='batchmean')(output_batch, teacher_outputs)
-
-        return loss
 
 
